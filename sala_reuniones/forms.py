a94d7019ec8f34f5e9774152e53563b2235950ef
bergaeduardo <bergaeduardo@gmail.com>
2022-02-10 20:24:35 +0100

    Cambios en diseño y validaciones

--- sala_reuniones/forms.py
+++ sala_reuniones/forms.py
@@ -1,7 +1,7 @@
 # -*- encoding:utf-8 -*-
 # -*- coding:utf-8 -*-
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -46,7 +46,7 @@ class ReservaForm(forms.ModelForm):
         fecha = self.cleaned_data['fecha']
 
         if fecha < datetime.now().date():
-            raise forms.ValidationError('Este campo no admite fechas en el pasado.')
+            raise forms.ValidationError('Este campo no admite fechas en el pasado.', code='invalid')
 
         return fecha
 
@@ -73,27 +73,59 @@ class ReservaForm(forms.ModelForm):
         except KeyError:
             return sala_reunion
 
+        
         for horario in sala_reunion.horario_disponibilidad.all():
             if hora_inicio < horario.hora_inicio or hora_inicio > horario.hora_fin:
                 self.add_error('hora_inicio', 'No hay disponibilidad a esta hora.')
             if hora_final < horario.hora_inicio or hora_final > horario.hora_fin:
                 self.add_error('hora_final', 'No hay disponibilidad en este horario.')
+                
+                
+
+        
 
         return sala_reunion
 
     def clean(self):
+        
 
         try:
             fecha = self.cleaned_data['fecha']
             hora_inicio = self.cleaned_data['hora_inicio']
             hora_final = self.cleaned_data['hora_final']
+            fechaForm = fecha
+            H_i=hora_inicio.hour
+            # H_i.isoformat(timespec='auto') 
 
         except KeyError:
             return self.cleaned_data
 
-        reserva = Reserva.objects.filter(fecha=fecha, hora_inicio=hora_inicio, hora_final=hora_final)
+        # reserva = Reserva.objects.filter(fecha=fecha, hora_inicio=hora_inicio, hora_final=hora_final)
+        cantReservas =Reserva.objects.filter(fecha=fechaForm, hora_final__gt=hora_inicio).count()
+        # T_reservas=cantReservas.filter(hora_final__lte=hora_final)
+        print('      *******<>*******       ')
+        # for r in cantReservas:
+        #     print(r.hora_inicio.hour)
+        #     print(type(r.hora_inicio))
+        #     print(r.hora_final)
+        #     print(type(r.hora_final))
+        print(cantReservas)
+        print('Fecha: ' + str(fechaForm))
+        print(hora_inicio)
+        # print(H_i)
+        # print(T_reservas)
+        print('Hora de fin:' + str(hora_final))
+        print('      ********<>******       ')
 
-        if reserva:
+        # if cantReservas > 0:
+        #     print('      *******<8>*******       ')
+        #     print(cantReservas)
+
+        if cantReservas >0:
+            print('      **************       ')
+            print(cantReservas)
+            print(fechaForm)
             raise forms.ValidationError('Ya existe una reserva en esa fecha y rango de horarios.')
+            
 
         return self.cleaned_data
--- sala_reuniones/views.py
+++ sala_reuniones/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from distutils import errors
+from distutils.log import ERROR
 
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, View, TemplateView, UpdateView, DeleteView
@@ -11,6 +13,8 @@ from .models import SalaReuniones, Reserva
 from .forms import UserRegisterForm, ReservaForm
 
 from braces.views import LoginRequiredMixin, FormValidMessageMixin
+from datetime import datetime, timedelta
+
 
 # Create your views here.
 
@@ -19,6 +23,14 @@ class HomeView(ListView):
     model = SalaReuniones
     template_name = 'sala_reuniones/home.html'
     context_object_name = 'salas_reuniones'
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super(HomeView, self).get_context_data(*args, **kwargs)
+        reserva=Reserva.objects.filter(fecha=datetime.now().date()).order_by('fecha')        
+        context_data['reservas'] = reserva
+        print(reserva)
+        
+        return context_data
 
 
 class RegisterView(View):
@@ -41,7 +53,7 @@ class RegisterView(View):
 class AdicionarReservaView(LoginRequiredMixin, CreateView):
     model = Reserva
     form_class = ReservaForm
-    success_url = reverse_lazy('sala_reuniones:listar_reservas')
+    success_url = reverse_lazy('sala_reuniones:calendario_reservas')
 
     def form_valid(self, form):
         formulario = form.save(commit=False)
@@ -60,8 +72,17 @@ class AdicionarReservaView(LoginRequiredMixin, CreateView):
     def get_context_data(self, *args, **kwargs):
         context_data = super(AdicionarReservaView, self).get_context_data(*args, **kwargs)
         sala_reunion = SalaReuniones.objects.get(pk=self.kwargs['pk'])
+        hoy = datetime.date(datetime.now())
+        Dia_reserva= hoy
+
+
         context_data['sala_reunion'] = sala_reunion
         context_data['horarios'] = sala_reunion.horario_disponibilidad.all()
+        context_data['aux'] = hoy
+        context_data['aux2'] = Dia_reserva
+        # error = ReservaForm.hora_final.error_messages
+        # print(error)
+        
 
         return context_data
 
@@ -69,6 +90,8 @@ class AdicionarReservaView(LoginRequiredMixin, CreateView):
         kwargs = super(AdicionarReservaView, self).get_form_kwargs()
         kwargs['pk'] = self.kwargs['pk']
         return kwargs
+
+    
 
 
 class ListarReservasViews(ListView):
@@ -90,18 +113,30 @@ class EditarReservaView(LoginRequiredMixin, FormValidMessageMixin, UpdateView):
     model = Reserva
     form_class = ReservaForm
     form_valid_message = 'Se ha editado la reserva satisfactoriamente.'
-    success_url = reverse_lazy('sala_reuniones:listar_reservas')
+    success_url = reverse_lazy('sala_reuniones:calendario_reservas')
 
     def get_context_data(self, *args, **kwargs):
         reserva = Reserva.objects.get(pk=self.kwargs['pk'])
+        hoy = datetime.date(datetime.now())
+        Dia_reserva = reserva.fecha
+
 
         context_data = super(EditarReservaView, self).get_context_data(*args, **kwargs)
         context_data['sala_reunion'] = reserva.sala_reuniones
         context_data['horarios'] = reserva.sala_reuniones.horario_disponibilidad.all()
         context_data['editar'] = True
         context_data['pk'] = self.object.pk
+        context_data['aux'] = hoy
+        context_data['aux2'] = Dia_reserva
+
+        
 
         return context_data
+
+    # @property
+    # def is_past_due(self):
+    #     aux = datetime.date.today()
+    #     return aux
 
 
 class EliminarReservasView(LoginRequiredMixin, DeleteView):
